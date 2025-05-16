f488559c0eaea91e097a16029a96a3a67fe8bf9f
nnalsarayy <n.alsaray@hotmail.com>
2025-05-16 18:17:49 +0200

    Update payment-callback.js

--- netlify/functions/payment-callback.js
+++ netlify/functions/payment-callback.js
@@ -1,1 +1,9 @@
+// payment-callback.js
+export const handler = async (event) => {
+  // Swish skickar sitt JSON-payload i POST-body
+  const { payeePaymentReference, status } = JSON.parse(event.body)
+  console.log('Swish callback:', payeePaymentReference, status)
+  // Här skulle du normalt uppdatera din databas, t.ex. via fetch/MongoDB/…
+  return { statusCode: 200 }
+}
 
